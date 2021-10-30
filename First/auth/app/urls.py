b55240f1c71881994cec5449e7a0528154629f5e
shubham thakur <79434430+shubhamthakur19@users.noreply.github.com>
2021-10-30 22:57:37 +0530

    Add files via upload

---
+++ First/auth/app/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from django.urls import path
+from .views import SignupView, GetCSRFToken, LoginView, LogoutView, CheckAuthenticatedView
+
+
+urlpatterns = [
+    path('authenticated', CheckAuthenticatedView.as_view()),
+    path('register', SignupView.as_view()),
+    path('login', LoginView.as_view()),
+    path('logout', LogoutView.as_view()),
+    path('csrf_cookie', GetCSRFToken.as_view()),
+]
